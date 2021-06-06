acd74c00437be92e3c609ae3615b8fc2fde420d8
idenc <iden.craven@gmail.com>
2021-06-06 12:57:09 -0600

    Update footer mountain animation to not be blurry on mobile

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,8 @@
 <template>
   <footer id="footer-root">
-    <div id="mountain1"></div>
-    <div id="mountain2"></div>
+    <Mountain />
     <div id="footer-content">
-      <img src="../assets/img/logo.png" />
+      <img src="../assets/img/logo.png" alt="logo" />
       <div class="vertical-divider"></div>
       <h1>Iden Craven</h1>
     </div>
@@ -11,8 +10,10 @@
 </template>
 
 <script>
+import Mountain from "@/components/Mountain";
 export default {
   name: "Footer",
+  components: { Mountain },
 };
 </script>
 
@@ -27,40 +28,6 @@ export default {
   grid-column-start: 1;
 }
 
-#mountain1,
-#mountain2 {
-  height: 19.688rem;
-  width: 8000px;
-}
-
-#mountain1 {
-  background: url("../assets/img/mountain.svg") repeat-x;
-  animation: mountain1Loop 200s linear infinite;
-}
-
-#mountain2 {
-  background: url("../assets/img/mountain.svg") repeat-x;
-  animation: mountain2Loop 150s linear infinite;
-}
-
-@keyframes mountain1Loop {
-  0% {
-    background-position: 0 0;
-  }
-  100% {
-    background-position: 8000px 0;
-  }
-}
-
-@keyframes mountain2Loop {
-  0% {
-    background-position: 500px 0;
-  }
-  100% {
-    background-position: 8000px 0;
-  }
-}
-
 #footer-content {
   display: flex;
   width: 100vw;
@@ -68,6 +35,7 @@ export default {
   align-items: center;
   flex-flow: row wrap;
   margin-top: 6em;
+  z-index: 4;
 }
 
 #footer-content img {
